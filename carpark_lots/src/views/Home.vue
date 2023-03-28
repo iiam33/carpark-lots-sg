f2054d5e3728eef1f3c30a518f2693c2e481acde
iiam33 <hongyoong.yeoh@gmail.com>
2023-03-28 18:29:08 +0800

    changed all the hard-coded data into dynamic data

--- carpark_lots/src/views/Home.vue
+++ carpark_lots/src/views/Home.vue
@@ -21,12 +21,12 @@
                   <strong>Small</strong>
                 </div>
                 <div class="column is-6 has-text-centered">
-                  1
+                  {{ this.counter.small }}
                 </div>
               </div>
               <!-- <br> -->
               <div class="pl-5 is-size-7 ">
-                Updated at: 11:09 PM - 1 Jan 2016
+                <strong>Updated at:</strong> {{ this.timestamp }}
               </div>
             </div>
           </div>
@@ -37,7 +37,18 @@
         <div class="card">
           <div class="card-content">
             <div class="content">
-              <strong>Medium</strong>
+              <div class="columns">
+                <div class="column is-6 has-text-centered">
+                  <strong>Medium</strong>
+                </div>
+                <div class="column is-6 has-text-centered">
+                  {{ this.counter.medium }}
+                </div>
+              </div>
+              <!-- <br> -->
+              <div class="pl-5 is-size-7 ">
+                <strong>Updated at:</strong> {{ this.timestamp }}
+              </div>
             </div>
           </div>
         </div>
@@ -47,7 +58,18 @@
         <div class="card">
           <div class="card-content">
             <div class="content">
-              <strong>Big</strong>
+              <div class="columns">
+                <div class="column is-6 has-text-centered">
+                  <strong>Big</strong>
+                </div>
+                <div class="column is-6 has-text-centered">
+                  {{ this.counter.big }}
+                </div>
+              </div>
+              <!-- <br> -->
+              <div class="pl-5 is-size-7 ">
+                <strong>Updated at:</strong> {{ this.timestamp }}
+              </div>
             </div>
           </div>
         </div>
@@ -57,7 +79,18 @@
         <div class="card">
           <div class="card-content">
             <div class="content">
-              <strong>Large</strong>
+              <div class="columns">
+                <div class="column is-6 has-text-centered">
+                  <strong>Large</strong>
+                </div>
+                <div class="column is-6 has-text-centered">
+                  {{ this.counter.large }}
+                </div>
+              </div>
+              <!-- <br> -->
+              <div class="pl-5 is-size-7 ">
+                <strong>Updated at:</strong> {{ this.timestamp }}
+              </div>
             </div>
           </div>
         </div>
@@ -76,7 +109,10 @@ export default {
   data() {
     return {
       carparkAvailability: [],
+      carparkData: [],
+      timestamp: "",
       counter: {
+        total: 0,
         small: 0, 
         medium:0,
         big: 0,
@@ -95,7 +131,33 @@ export default {
         .get('/v1/transport/carpark-availability')
         .then(response => {
           this.carparkAvailability = response.data.items
-          console.log(this.carparkAvailability)
+          var oldTimestamp = this.carparkAvailability[0].timestamp
+          var datetime = new Date(oldTimestamp)
+          this.timestamp = datetime.toString()
+          this.carparkData = this.carparkAvailability[0].carpark_data
+          if (this.carparkData) {
+            this.carparkData.filter(
+              (data) => data.carpark_info.filter(
+                (info) => {
+                  if (parseInt(info.total_lots) >= 0 && parseInt(info.total_lots) < 100) {
+                    this.counter.small += 1
+                  }
+                  if (parseInt(info.total_lots) >= 100 && parseInt(info.total_lots) < 300) {
+                    this.counter.medium += 1
+                  }
+                  if (parseInt(info.total_lots) >= 300 && parseInt(info.total_lots) < 400) {
+                    this.counter.big += 1
+                  }
+                  if (parseInt(info.total_lots) >= 400) {
+                    this.counter.large += 1
+                  }
+                  if (parseInt(info.total_lots) >= 0) {
+                    this.counter.total += 1
+                  }
+                }
+                ).length
+            )
+          }
         })
         .catch(error => {
           console.log(error)
