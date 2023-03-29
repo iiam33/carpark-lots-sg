4f464b5276df460b0121d56f363d82361d6051c5
iiam33 <hongyoong.yeoh@gmail.com>
2023-03-29 08:28:16 +0800

    done creating the max and min for all category of carparks

--- carpark_lots/src/store/index.js
+++ carpark_lots/src/store/index.js
@@ -3,21 +3,137 @@ import axios from 'axios'
 
 const store = createStore({
   state: {
-    counter: {
-      total: 0,
-      small: 0, 
-      medium:0,
-      big: 0,
-      large: 0,
+    carparkDetail: {
+      total: [],
+      small: [], 
+      medium:[],
+      big: [],
+      large: [],
     },
     timestamp: "",
   },
   getters: {
+    totalSmallCarpark(state) {
+      return state.carparkDetail.small.length
+    },
+    totalMediumCarpark(state) {
+      return state.carparkDetail.medium.length
+    },
+    totalBigCarpark(state) {
+      return state.carparkDetail.big.length
+    },
+    totalLargeCarpark(state) {
+      return state.carparkDetail.large.length
+    },
+    totalCarpark(state) {
+      return state.carparkDetail.total.length
+    },
+    totalMinMaxSmallCarpark(state) {
+      var carparkDetail = {
+        minCarparkNum: [],
+        maxCarparkNum: []
+      }
+      var max = state.carparkDetail.small.reduce(
+        (prev, curr) => prev.lots_available > curr.lots_available ? prev : curr, 0
+      )
+      var min = state.carparkDetail.small.reduce(
+        (prev, curr) => prev.lots_available < curr.lots_available ? prev : curr, 0
+      )
+      state.carparkDetail.small.filter((carpark) => {
+        if (carpark.lots_available >= max.lots_available) 
+          carparkDetail.maxCarparkNum.push(carpark.carpark_number)
+        
+        if (carpark.lots_available <= min.lots_available)
+          carparkDetail.minCarparkNum.push(carpark.carpark_number)
+          
+      })
+      carparkDetail.max = max.lots_available
+      carparkDetail.min = min.lots_available
+      console.log('small carpark', carparkDetail)
+
+      return carparkDetail
+    },
+    totalMinMaxMediumCarpark(state) {
+      var carparkDetail = {
+        minCarparkNum: [],
+        maxCarparkNum: []
+      }
+      var max = state.carparkDetail.medium.reduce(
+        (prev, curr) => prev.lots_available > curr.lots_available ? prev : curr, 0
+      )
+      var min = state.carparkDetail.medium.reduce(
+        (prev, curr) => prev.lots_available < curr.lots_available ? prev : curr, 0
+      )
+      state.carparkDetail.medium.filter((carpark) => {
+        if (carpark.lots_available >= max.lots_available) 
+          carparkDetail.maxCarparkNum.push(carpark.carpark_number)
+        
+        if (carpark.lots_available <= min.lots_available)
+          carparkDetail.minCarparkNum.push(carpark.carpark_number)
+          
+      })
+      carparkDetail.max = max.lots_available
+      carparkDetail.min = min.lots_available
+      console.log('medium carpark', carparkDetail)
+
+      return carparkDetail
+    },
+    totalMinMaxBigCarpark(state) {
+      var carparkDetail = {
+        minCarparkNum: [],
+        maxCarparkNum: []
+      }
+      var max = state.carparkDetail.big.reduce(
+        (prev, curr) => prev.lots_available > curr.lots_available ? prev : curr, 0
+      )
+      var min = state.carparkDetail.big.reduce(
+        (prev, curr) => prev.lots_available < curr.lots_available ? prev : curr, 0
+      )
+      state.carparkDetail.big.filter((carpark) => {
+        if (carpark.lots_available >= max.lots_available) 
+          carparkDetail.maxCarparkNum.push(carpark.carpark_number)
+        
+        if (carpark.lots_available <= min.lots_available)
+          carparkDetail.minCarparkNum.push(carpark.carpark_number)
+          
+      })
+      carparkDetail.max = max.lots_available
+      carparkDetail.min = min.lots_available
+      console.log('big carpark', carparkDetail)
+
+      return carparkDetail
+    },
+    totalMinMaxLargeCarpark(state) {
+      var carparkDetail = {
+        minCarparkNum: [],
+        maxCarparkNum: []
+      }
+      var max = state.carparkDetail.large.reduce(
+        (prev, curr) => prev.lots_available > curr.lots_available ? prev : curr, 0
+      )
+      var min = state.carparkDetail.large.reduce(
+        (prev, curr) => prev.lots_available < curr.lots_available ? prev : curr, 0
+      )
+      state.carparkDetail.large.filter((carpark) => {
+        if (carpark.lots_available >= max.lots_available) 
+          carparkDetail.maxCarparkNum.push(carpark.carpark_number)
+        
+        if (carpark.lots_available <= min.lots_available)
+          carparkDetail.minCarparkNum.push(carpark.carpark_number)
+          
+      })
+      carparkDetail.max = max.lots_available
+      carparkDetail.min = min.lots_available
+      console.log('large carpark', carparkDetail)
+
+      return carparkDetail
+    },
+    
   },
   mutations: {
-    setCounter(state, counter) {
-      console.log(counter)
-      state.counter = counter
+    setCarparkDetail(state, carparkDetail) {
+      console.log(carparkDetail)
+      state.carparkDetail = carparkDetail
     },
     setTimestamp(state, timestamp) {
       state.timestamp = timestamp
@@ -28,41 +144,47 @@ const store = createStore({
       axios
         .get('/v1/transport/carpark-availability')
         .then((response) => {
-          var new_counter = {
-            total: 0,
-            small: 0, 
-            medium:0,
-            big: 0,
-            large: 0,
+          var newCarparkDetail = {
+            total: [],
+            small: [], 
+            medium:[],
+            big: [],
+            large: [],
           }
+
           var oldTimestamp = response.data.items[0].timestamp
           var datetime = new Date(oldTimestamp)
           commit("setTimestamp", datetime.toString())
-          this.carparkData = response.data.items[0].carpark_data
-          if (this.carparkData) {
-            this.carparkData.filter(
+          var carparkData = response.data.items[0].carpark_data
+          if (carparkData) {
+            carparkData.filter(
               (data) => data.carpark_info.filter(
                 (info) => {
                   if (parseInt(info.total_lots) >= 0 && parseInt(info.total_lots) < 100) {
-                    new_counter.small += 1
+                    info.carpark_number = data.carpark_number
+                    newCarparkDetail.small.push(info)
                   }
                   if (parseInt(info.total_lots) >= 100 && parseInt(info.total_lots) < 300) {
-                    new_counter.medium += 1
+                    info.carpark_number = data.carpark_number
+                    newCarparkDetail.medium.push(info)
                   }
                   if (parseInt(info.total_lots) >= 300 && parseInt(info.total_lots) < 400) {
-                    new_counter.big += 1
+                    info.carpark_number = data.carpark_number
+                    newCarparkDetail.big.push(info)
                   }
                   if (parseInt(info.total_lots) >= 400) {
-                    new_counter.large += 1
+                    info.carpark_number = data.carpark_number
+                    newCarparkDetail.large.push(info)
                   }
                   if (parseInt(info.total_lots) >= 0) {
-                    new_counter.total += 1
+                    info.carpark_number = data.carpark_number
+                    newCarparkDetail.total.push(info)
                   }
                 }
                 ).length
             )
           }
-          commit("setCounter", new_counter)
+          commit("setCarparkDetail", newCarparkDetail)
         })
         .catch(error => {
           console.log(error)
--- carpark_lots/src/views/Home.vue
+++ carpark_lots/src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <section class="hero is-medium is-dark mb-6">
       <div class="hero-body has-text-centered">
-          <p class="title">Welcome to Carpark Lots Counter in Singapore</p>
+          <p class="title">Welcome to Carpark Availability Checker in Singapore</p>
       </div>
     </section>
 
-    <div class="columns is-multiline">
+    <div class="columns is-multiline mb-6">
       <div class="column is-12">
         <div class="is-size-2 has-text-centered">Carpark Category</div>
       </div>
 
-      <!-- <div class="column is-3" v-for="category in carparkCategory" v-bind:key="category"> -->
+      <div class="column is-12 mb-5">
+        <div class="is-size-5 has-text-centered">Total Number of Carpark Available: {{ store.getters.totalCarpark }}</div>
+      </div>
+
       <div class="column is-3">
         <div class="card">
           <div class="card-content">
@@ -21,10 +24,10 @@
                   <strong>Small</strong>
                 </div>
                 <div class="column is-6 has-text-centered">
-                  {{ store.state.counter.small }}
+                  {{ store.getters.totalSmallCarpark }}
                 </div>
               </div>
-              <!-- <br> -->
+
               <div class="pl-5 is-size-7 ">
                 <strong>Updated at:</strong> {{ store.state.timestamp }}
               </div>
@@ -42,10 +45,10 @@
                   <strong>Medium</strong>
                 </div>
                 <div class="column is-6 has-text-centered">
-                  {{ store.state.counter.medium }}
+                  {{ store.getters.totalMediumCarpark }}
                 </div>
               </div>
-              <!-- <br> -->
+
               <div class="pl-5 is-size-7 ">
                 <strong>Updated at:</strong> {{ store.state.timestamp }}
               </div>
@@ -63,10 +66,10 @@
                   <strong>Big</strong>
                 </div>
                 <div class="column is-6 has-text-centered">
-                  {{ store.state.counter.big }}
+                  {{ store.getters.totalBigCarpark }}
                 </div>
               </div>
-              <!-- <br> -->
+
               <div class="pl-5 is-size-7 ">
                 <strong>Updated at:</strong> {{ store.state.timestamp }}
               </div>
@@ -84,10 +87,10 @@
                   <strong>Large</strong>
                 </div>
                 <div class="column is-6 has-text-centered">
-                  {{ store.state.counter.large }}
+                  {{ store.getters.totalLargeCarpark }}
                 </div>
               </div>
-              <!-- <br> -->
+
               <div class="pl-5 is-size-7 ">
                 <strong>Updated at:</strong> {{ store.state.timestamp }}
               </div>
@@ -95,19 +98,32 @@
           </div>
         </div>
       </div>
+    </div>
 
+    <hr>
+    <div class="columns is-multiline mb-6">
+      <div class="column is-12">
+        <div class="is-size-2 has-text-centered">Carpark Category</div>
+      </div>
+
+      <div class="column is-12 mb-5">
+        <div class="is-size-7 has-text-centered">Total Number of Small Carpark Available: {{ smallCarpark.max }} </div>
+        <div class="is-size-7 has-text-centered">Total Number of Medium Carpark Available: {{ mediumCarpark.max }} </div>
+        <div class="is-size-7 has-text-centered">Total Number of Big Carpark Available: {{ bigCarpark.max }} </div>
+        <div class="is-size-7 has-text-centered">Total Number of Large Carpark Available: {{ largeCarpark.max }} </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { useStore } from 'vuex'
+import { computed } from 'vue'
 
 export default {
   name: 'Home',
   data() {
     return {
-      carparkAvailability: [],
       timestamp: "",
       timer: null,
     }
@@ -125,12 +141,21 @@ export default {
   setup() {
     const store = useStore()
 
+    const smallCarpark = computed(() => store.getters.totalMinMaxSmallCarpark)
+    const mediumCarpark = computed(() => store.getters.totalMinMaxMediumCarpark)
+    const bigCarpark = computed(() => store.getters.totalMinMaxBigCarpark)
+    const largeCarpark = computed(() => store.getters.totalMinMaxLargeCarpark)
+
     function getCarparkAvailability() {
       store.dispatch('getCarparkAvailability')
     }
 
     return {
       store,
+      smallCarpark,
+      mediumCarpark,
+      bigCarpark,
+      largeCarpark,
       getCarparkAvailability
     }
   }
